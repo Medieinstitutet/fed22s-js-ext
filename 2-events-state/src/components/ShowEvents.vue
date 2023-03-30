a6a42acf373e0a438a5e646344dc857603db8ce0
Sebastian Tegel <sebastian.tegel@tegelconsulting.se>
2023-03-30 11:06:35 +0200

    Added project

---
+++ 2-events-state/src/components/ShowEvents.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const handleClick = (age: number, event: MouseEvent) => {
+  console.log("Du klickade på knappen", age, event);
+};
+
+const handleSubmit = () => {};
+</script>
+
+<template>
+  <button @click.once="(event) => handleClick(43, event)">Show age</button>
+  <button @click="handleClick(43, $event)">Show age - again</button>
+
+  <button v-on:click="">Test button - does nothing</button>
+
+  <form @submit.prevent="handleSubmit">
+    <input type="text" name="firstname" />
+
+    <button>Spara</button>
+  </form>
+</template>
---
+++ 2-events-state/src/components/ShowState.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { reactive, ref } from "vue";
+import { Person } from "../models/Person";
+
+// let age = 43;
+const state = reactive({ age: 43, name: "Sebastian", isMarried: true });
+const state2 = ref<Person[]>([{ name: "Sebastian", age: 43, isMarried: true }]);
+
+const handleClick = () => {
+  //   if (state2.value.age < 49) state2.value.age++;
+  //   console.log(state2.value);
+
+  state2.value.push(new Person("Alvar", 12, false));
+};
+
+const removePerson = (i: number) => {
+  state2.value.splice(i, 1);
+};
+</script>
+
+<template>
+  <p v-for="(person, index) in state2" :key="index">
+    {{ person.name }} - {{ person.age }}
+    <button @click="removePerson(index)">Ta bort</button>
+  </p>
+
+  <button @click="handleClick">Lägg till person</button>
+</template>
