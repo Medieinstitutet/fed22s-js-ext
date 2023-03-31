ca112c61a5869cd92102c79311e2c472f29f7b75
Sebastian Tegel <sebastian.tegel@tegelconsulting.se>
2023-03-31 11:14:15 +0200

    Added project

---
+++ 3-style-form/src/App.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import ShowTodos from "./components/ShowTodos.vue";
+import { Person } from "./models/Person";
+
+// const firstname = ref("");
+// const age = ref(0);
+// const isMarried = ref(false);
+
+const state = ref<Person>({ name: "", age: 0, isMarried: false });
+const persons = ref<Person[]>([]);
+
+function handleClick() {
+  console.log(state.value);
+  persons.value.push(
+    new Person(state.value.name, state.value.age, state.value.isMarried)
+  );
+  state.value = { name: "", age: 0, isMarried: false };
+}
+</script>
+
+<template>
+  <!-- <input type="text" v-model="state.name" />
+  <input type="number" v-model="state.age" />
+  <input type="checkbox" v-model="state.isMarried" />
+
+  <button @click="handleClick">Spara</button>
+
+  <p>{{ state }}</p>
+
+  <div
+    v-for="(person, index) in persons"
+    :key="index"
+    :class="person.age > 20 ? 'old' : 'young'"
+  >
+    {{ person.name }} - {{ person.age }}
+  </div> -->
+
+  <ShowTodos></ShowTodos>
+</template>
+
+<style scoped>
+.young {
+  background-color: green;
+}
+
+.middle {
+  background-color: yellow;
+}
+
+.old {
+  background-color: plum;
+}
+</style>
---
+++ 3-style-form/src/components/ShowTodos.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { Todo } from "../models/Todo";
+
+const todos = ref<Todo[]>([]);
+
+const userInput = ref("");
+
+const toggleTodo = (i: number) => {
+  todos.value[i].done = !todos.value[i].done;
+};
+
+const handleSubmit = () => {
+  todos.value.push(new Todo(userInput.value, false));
+  userInput.value = "";
+};
+
+const removeTodo = (i: number) => {
+  todos.value.splice(i, 1);
+};
+</script>
+
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input type="text" v-model="userInput" />
+  </form>
+
+  <ul>
+    <li
+      v-for="(todo, index) in todos"
+      :key="index"
+      class="todo"
+      :class="todo.done ? 'done' : ''"
+    >
+      {{ todo.text }}
+      <button @click="toggleTodo(index)">Toggle</button>
+      <button @click="removeTodo(index)">Remove</button>
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+.todo {
+}
+
+.todo.done {
+  text-decoration: line-through;
+}
+</style>
